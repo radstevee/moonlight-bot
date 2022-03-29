6a3aff24e50e1985f01e44b62b4e56d7030363b8
Radsteve <96977790+radstevee@users.noreply.github.com>
2022-03-29 17:47:32 +0200

    changes
    yep, im the annoying git dev

--- commands/fun/cat.js
+++ commands/fun/cat.js
@@ -7,7 +7,6 @@ module.exports = {
         let req = await fetch('https://api.thecatapi.com/v1/images/search').catch(console.log);
         if(!req) return message.channel.send('An error appeared.');
         let res = await req.json();
-        console.log(res[0].url)
         let embed = {
             image: {
                 url: res[0].url
